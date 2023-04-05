d1dc20f7b600f686ec643e85dc258ed86a6d7346
clavay <clavayssiere@univ-pau.fr>
2023-04-05 11:41:23 +0200

    add project informations
    use project informations
    update license and pyscada version

--- pyscada/scripting/__init__.py
+++ pyscada/scripting/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import pyscada
-
-__version__ = '0.7.1rc1'
-__author__ = 'Martin Schröder'
+__version__ = "0.8.0"
+__author__ = "Martin Schröder, Camille Lavayssiere"
+__email__ = "team@pyscada.org"
+__description__ = "Scripting extension for PyScada a Python and Django based Open Source SCADA System"
+__app_name__ = "Scripting"
 
 parent_process_list = [{'pk': 95,
-                        'label': 'pyscada.scripting',
-                        'process_class': 'pyscada.scripting.worker.MasterProcess',
+                        'label': 'pyscada.' + __app_name__.lower(),
+                        'process_class': 'pyscada.' + __app_name__.lower() + '.worker.Process',
                         'process_class_kwargs': '{"dt_set":30}',
                         'enabled': True}]
