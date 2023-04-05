d9bdc199480b9e2006f3e22b9ee3cb846d301753
clavay <clavayssiere@univ-pau.fr>
2023-04-05 15:40:50 +0200

    fix scripting master process class name

--- pyscada/scripting/__init__.py
+++ pyscada/scripting/__init__.py
@@ -9,6 +9,6 @@ __app_name__ = "Scripting"
 
 parent_process_list = [{'pk': 95,
                         'label': 'pyscada.' + __app_name__.lower(),
-                        'process_class': 'pyscada.' + __app_name__.lower() + '.worker.Process',
+                        'process_class': 'pyscada.' + __app_name__.lower() + '.worker.MasterProcess',
                         'process_class_kwargs': '{"dt_set":30}',
                         'enabled': True}]
